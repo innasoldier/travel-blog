7afbbc6c265d55aaf0c57dd07c32e3f0b1fa4fd9
Inna Soldatkina <inna.soldier@gmail.com>
2022-09-26 20:59:37 -0500

    added capstone to git

---
+++ old_ie.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+  document.createElement('header');
+  document.createElement('nav');
+  document.createElement('footer');
+  document.createElement('article');
+  document.createElement('section');
+  document.createElement('main');
